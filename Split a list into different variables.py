966975cce17211c29d0073244efb7a954ace57a9
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2025-08-01 19:32:46 +0530

    Add files via upload

---
+++ Split a list into different variables.py
@@ -0,0 +1,7 @@
+#Write a PYTHON program to split a list into different variables
+
+lst = [word for word in input("Please enter a line of text: ").split()]
+print("-"*50)
+print(lst)
+print("-"*50)
+print("Program executed successfully")
