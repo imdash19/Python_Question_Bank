b80ba02ba41660c20e31a2608320c9fd2c192742
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2025-08-01 19:55:41 +0530

    Delete Split a list based on first character of word.py

--- Split a list based on first character of word.py
+++
@@ -1,9 +0,0 @@
-#Write a PYTHON program to split a list based on first character of word.
-
-lst = [word for word in input("Please enter a line of text: ").split()]
-print("-"*50)
-
-print(lst)
-print("-"*50)
-
-print("Program executed successfully")
