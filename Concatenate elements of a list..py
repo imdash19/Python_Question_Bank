23414a536a070e1e98e4a856f60ee7ca1a069711
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2025-08-06 07:40:21 +0530

    Add files via upload

---
+++ Concatenate elements of a list..py
@@ -0,0 +1,20 @@
+# Write a PYTHON program to concatenate elements of a list.
+
+lst = [val for val in input("Please enter your values with space: ").split()]
+print("-"*70)
+print(lst)
+print("-"*70)
+
+if(len(lst) == 0):
+    print("Please enter a valid input")
+    print("-"*70)
+
+else:
+    string = ""
+    for ele in lst:
+        string += ele
+
+    print(string)
+    print("-" * 70)
+
+print("Program executed successfully")
