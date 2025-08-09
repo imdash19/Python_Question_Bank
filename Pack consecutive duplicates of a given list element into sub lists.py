08d78887d4357510a43416a43441129d49794676
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2025-08-09 20:50:44 +0530

    Add files via upload

---
+++ Pack consecutive duplicates of a given list element into sub lists.py
@@ -0,0 +1,48 @@
+#   Write a PYTHON program to pack consecutive duplicates of a given list element into sub lists.
+# 	Original list:
+# 	[0, 0, 1, 2, 3, 4, 4, 5, 6, 6, 6, 7, 8, 9, 4, 4]
+# 	After packing consecutive duplicates of the said list elements into sub lists:
+# 	[[0, 0], [1], [2], [3], [4, 4], [5], [6, 6, 6], [7], [8], [9], [4, 4]]
+
+try:
+    plst = []
+    elst = []
+    lst = [val for val in input("Please enter your numbers separated with space: ").split()]
+    print("-"*70)
+
+    if(len(lst) == 0):
+        print("Please enter a valid input.")
+        print("-" * 70)
+
+    else:
+        slst = []
+        for i in range(0, len(lst)):
+            if(lst[i] in slst):
+                slst.append(lst[i])
+
+            else:
+                slst = []
+                slst.append(lst[i])
+
+            plst.append(slst)
+
+        for i in range(0, len(plst)):
+            if(plst[i] != plst[i-1]):
+                elst.append(plst[i])
+
+except ValueError as v:
+    print("-"*70)
+    print("Please enter a number: ", v)
+    print("-" * 70)
+
+except Exception as e:
+    print("-" * 70)
+    print("Something went wrong! Please check again. ", e)
+    print("-" * 70)
+
+else:
+    print("After packing consecutive duplicates of the said list elements into sub lists: ", elst)
+    print("-"*70)
+
+finally:
+    print("Program executed successfully")
