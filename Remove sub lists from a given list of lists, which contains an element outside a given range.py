577718e0a9eb31eea38893e28c735c8320b29b44
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2025-08-15 20:48:59 +0530

    Update Remove sub lists from a given list of lists, which contains an element outside a given range.py

--- Remove sub lists from a given list of lists, which contains an element outside a given range.py
+++ Remove sub lists from a given list of lists, which contains an element outside a given range.py
@@ -5,56 +5,59 @@
 class ZeroNegativeError(Exception):
     pass
 
-try:
-    lst = []
-    result = []
-    n = int(input("Please enter how many sub lists you want: "))
-    print("-"*70)
-
-    if(n <= 0):
-        raise ZeroNegativeError
-
+    while True:
+    try:
+        lst = []
+        result = []
+        n = int(input("Please enter how many sub lists you want: "))
+        print("-"*70)
+    
+        if(n <= 0):
+            raise ZeroNegativeError
+    
+        else:
+            for i in range(1, n+1):
+                print("Inside {} list.".format(i))
+                print("-" * 70)
+                slst = [int(val) for val in input("Please enter your elements separate with space: ").split()]
+                print("-" * 70)
+                lst.append(slst)
+    
+            start = int(input("Please enter your starting index: "))
+            print("-" * 70)
+            end = int(input("Please enter your ending index: "))
+            print("-" * 70)
+    
+            for sele in lst:
+                for val in sele:
+                    if(val not in range(start, end+1)):
+                        break
+                else:
+                    print(sele)
+                    result.append(sele)
+    
+    except ZeroNegativeError:
+        print("Please don't enter ZERO or NEGATIVE number for input. Try again.")
+        print("-" * 70)
+    
+    except ValueError as v:
+        print("-" * 70)
+        print("Please don't enter ALPHABETS, ALPHANUMERICS, SPECIAL SYMBOLS for input. Try again.", v)
+        print("-" * 70)
+    
+    except Exception as e:
+        print("-" * 70)
+        print("Something went wrong! Check again.", e)
+        print("-" * 70)
+    
     else:
-        for i in range(1, n+1):
-            print("Inside {} list.".format(i))
-            print("-" * 70)
-            slst = [int(val) for val in input("Please enter your elements separate with space: ").split()]
-            print("-" * 70)
-            lst.append(slst)
-
-        start = int(input("Please enter your starting index: "))
+        print("Original list: ", lst)
         print("-" * 70)
-        end = int(input("Please enter your ending index: "))
+        print("After removing sub lists from a given list of lists, which contains an element outside the given range: ", result)
         print("-" * 70)
+        break
+    
+    finally:
+        print("Program executed successfully.")
 
-        for sele in lst:
-            for val in sele:
-                if(val not in range(start, end+1)):
-                    break
-            else:
-                print(sele)
-                result.append(sele)
-
-except ZeroNegativeError:
-    print("Please don't enter ZERO or NEGATIVE number for input. Try again.")
-    print("-" * 70)
-
-except ValueError as v:
-    print("-" * 70)
-    print("Please don't enter ALPHABETS, ALPHANUMERICS, SPECIAL SYMBOLS for input. Try again.", v)
-    print("-" * 70)
-
-except Exception as e:
-    print("-" * 70)
-    print("Something went wrong! Check again.", e)
-    print("-" * 70)
-
-else:
-    print("Original list: ", lst)
-    print("-" * 70)
-    print("After removing sub lists from a given list of lists, which contains an element outside the given range: ", result)
-    print("-" * 70)
-
-finally:
-    print("Program executed successfully.")
     print("-" * 70)
