fb075171c9e03f114953ce91256e8904c8e59d37
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-01-19 09:06:15 +0530

    Add files via upload

---
+++ Get the maximum value of a list, after mapping each element to a value using a given function.py
@@ -0,0 +1,27 @@
+# 	Write a Python program to get the maximum value of a list, after mapping each element to a value using a given function.
+# Sample Output: 8
+
+def max_after_mapping(lst, func):
+    return max(map(func, lst))
+
+lst = list(map(int, input("Enter list elements: ").split()))
+
+print("Choose mapping function:")
+print("1. Double the value")
+print("2. Square the value")
+print("3. Absolute value")
+
+choice = int(input("Enter choice (1/2/3): "))
+
+if choice == 1:
+    func = lambda x: x * 2
+elif choice == 2:
+    func = lambda x: x * x
+elif choice == 3:
+    func = lambda x: abs(x)
+else:
+    print("Invalid choice")
+    exit()
+
+result = max_after_mapping(lst, func)
+print(result)
