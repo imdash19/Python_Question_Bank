b75c04145d4d9698bb62405f53c2bc50d12dbbec
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-01-25 12:05:30 +0530

    Add files via upload

---
+++ Accepts a sequence of comma-separated numbers from the user and generates a list and a tuple of those numbers.py
@@ -0,0 +1,13 @@
+# Write a Python program that accepts a sequence of comma-separated numbers from the user and generates a list and a tuple of those numbers.
+# Sample data: 3, 5, 7, 23
+# Output:
+# List: ['3', ' 5', ' 7', ' 23']
+# Tuple: ('3', ' 5', ' 7', ' 23')
+
+def generate_list_tuple():
+    lst= [val for val in input('Please enter your values separated with comma: ').split(',')]
+    print('='*60)
+    print(lst)
+    print(tuple(lst))
+
+generate_list_tuple()
