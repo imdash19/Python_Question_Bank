1dccbf0e11f4c6978f7f02ebaf04df96068810a1
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-01-27 19:13:09 +0530

    Add files via upload

---
+++ Difference between a given number.py
@@ -0,0 +1,8 @@
+# Write a Python program to calculate the difference between a given number.
+
+def difference(num1, num2):
+    return num1 - num2
+
+num1= float(input('Please enter your first value: '))
+num2 = float(input('Please enter your second value: '))
+print(difference(num1, num2))
