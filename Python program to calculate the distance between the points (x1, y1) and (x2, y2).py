8b69c3fa592e02ef527df76b2155471451b6104d
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-02-01 16:03:37 +0530

    Add files via upload

---
+++ Python program to calculate the distance between the points (x1, y1) and (x2, y2).py
@@ -0,0 +1,13 @@
+# Write a Python program to calculate the distance between the points (x1, y1) and (x2, y2).
+
+import math
+
+def calculate_distance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+x1= float(input("Enter x1:"))
+y1= float(input("Enter y1:"))
+x2= float(input("Enter x2:"))
+y2= float(input("Enter y2:"))
+
+print(calculate_distance(x1, y1, x2, y2))
