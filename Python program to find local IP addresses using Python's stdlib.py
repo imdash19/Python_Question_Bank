367e94906543453ad4d233f0e8d652d67eeb401b
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-02-04 13:03:51 +0530

    Add files via upload

---
+++ Python program to find local IP addresses using Python's stdlib.py
@@ -0,0 +1,15 @@
+# Write a Python program to find local IP addresses using Python's stdlib.
+
+import socket
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip
+
+if __name__ == "__main__":
+    print("Local IP address:", get_local_ip())
