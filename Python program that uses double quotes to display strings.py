1c21a9de04604f13c2184b4c39e65e559da4bde8
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-02-18 15:33:14 +0530

    Add files via upload

---
+++ Python program that uses double quotes to display strings.py
@@ -0,0 +1,7 @@
+# Write a Python program that uses double quotes to display strings.
+
+def display_with_double_quotes():
+    text = input("Enter a string: ")
+    print("=" * 50)
+    print("\"" + text + "\"")
+display_with_double_quotes()
