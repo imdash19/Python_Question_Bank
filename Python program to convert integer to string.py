be4e81f24331c429e818e34e6397bb483e8fe64f
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-03-24 16:43:26 +0530

    Add files via upload

---
+++ Python program to convert integer to string.py
@@ -0,0 +1,13 @@
+# Write a Python program to convert integer to string.
+# Sample Input:
+# language = "Python"
+# version = 3.6
+# sample Output:
+# Language: Python, Version: 3.6
+
+language = input()
+version = float(input())
+
+version_str = str(version)
+
+print("Language:", language + ", Version:", version_str)
