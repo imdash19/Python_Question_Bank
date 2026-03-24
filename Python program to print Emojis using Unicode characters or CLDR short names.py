44f8fd7a6c4f7afc7e54a02946925f9c980eeddd
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-03-24 16:42:15 +0530

    Add files via upload

---
+++ Python program to print Emojis using Unicode characters or CLDR short names.py
@@ -0,0 +1,7 @@
+# Write a Python program to print Emojis using Unicode characters or CLDR (Common Locale Data Repository) short names.
+
+print("\U0001F600")
+print("\U0001F602")
+print("\U0001F60D")
+print("\U0001F680")
+print("\U0001F44D")
