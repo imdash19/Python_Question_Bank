d3fce73d8862f9b2db7212f4edca282aaec38e8b
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-03-31 11:16:46 +0530

    Add files via upload

---
+++ Python program to find x that minimizes the mean squared deviation from a given list of numbers.py
@@ -0,0 +1,12 @@
+# Write a Python program to find x that minimizes the mean squared deviation from a given list of numbers.
+# Input:
+# [4, -5, 17, -9, 14, 108, -9]
+# Output:
+# 17.142857142857142
+# Input:
+# [12, -2, 14, 3, -15, 10, -45, 3, 30]
+# Output:
+# 1.1111111111111112
+
+lst= [int(val) for val in input().split(', ') if (val.lstrip('-')).isdigit()]
+print(sum(lst)/len(lst))
