130f7fc0dc11fb021529fb27a75ac04acbc2e222
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-04-01 11:17:19 +0530

    Add files via upload

---
+++ Python program to rescale and shift numbers in a given list, so that they cover the range [0, 1].py
@@ -0,0 +1,18 @@
+# Write a Python program to rescale and shift numbers in a given list, so that they cover the range [0, 1].
+# Input:
+# [18.5, 17.0, 18.0, 19.0, 18.0]
+# Output:
+# [0.75, 0.0, 0.5, 1.0, 0.5]
+# Input:
+# [13.0, 17.0, 17.0, 15.5, 2.94]
+# Output:
+# [0.7155049786628734, 1.0, 1.0, 0.8933143669985776, 0.0]
+
+lst = list(map(float, input().strip('[]').split(',')))
+
+mn = min(lst)
+mx = max(lst)
+
+res = [(x - mn) / (mx - mn) for x in lst]
+
+print(res)
