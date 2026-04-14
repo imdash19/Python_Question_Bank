67ee34565c41c382d8c05ff1363119a1ebd9e68b
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-04-14 11:35:34 +0530

    Add files via upload

---
+++ Python program to count the number of characters (character frequency) in a string.py
@@ -0,0 +1,11 @@
+# Python program to count the number of characters (character frequency) in a string.
+# Sample String: google.com'
+# Expected Result: {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}
+
+s= input('Enter your string: ')
+d= {}
+
+for v in s:
+    d[v]= s.count(v)
+    
+print(d)
