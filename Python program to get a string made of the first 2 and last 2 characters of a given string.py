a7ca467e18a061c0c232cffc5d23b545719c90b7
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-04-15 11:48:49 +0530

    Add files via upload

---
+++ Python program to get a string made of the first 2 and last 2 characters of a given string.py
@@ -0,0 +1,11 @@
+# Write a Python program to get a string made of the first 2 and last 2 characters of a given string. If the string length is less than 2, return the empty string instead.
+# Sample String : 'w3resource'
+# Expected Result : 'w3ce'
+# Sample String : 'w3'
+# Expected Result : 'w3w3'
+# Sample String : ' w'
+# Expected Result : Empty String
+
+s= input()
+ns= s[:2] + s[-2:]
+print(ns)
