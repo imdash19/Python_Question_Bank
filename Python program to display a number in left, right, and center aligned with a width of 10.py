cbf49325f25c90a276729df6b1e71cab1344e3bb
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-04-21 11:37:35 +0530

    Add files via upload

---
+++ Python program to display a number in left, right, and center aligned with a width of 10.py
@@ -0,0 +1,7 @@
+# Write a Python program to display a number in left, right, and center aligned with a width of 10.
+
+num = int(input())
+
+print(f"{num:<10}")
+print(f"{num:>10}")
+print(f"{num:^10}")
