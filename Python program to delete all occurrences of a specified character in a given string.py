85e7d99355aa457b4ac2732738c143259ff32f4f
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-05-01 12:27:44 +0530

    Add files via upload

---
+++ Python program to delete all occurrences of a specified character in a given string.py
@@ -0,0 +1,10 @@
+# Write a Python program to delete all occurrences of a specified character in a given string.
+# Sample Output:
+# Original string:
+# Delete all occurrences of a specified character in a given string
+# Modified string:
+# Delete ll occurrences of specified chrcter in given string
+
+s = input()
+ch = input()
+print(s.replace(ch, ""))
