f861255c4626de8933e276e3364d559db64c16ce
Bibhuti Bhusan Dash <151985681+imdash19@users.noreply.github.com>
2026-05-05 12:19:06 +0530

    Add files via upload

---
+++ Python program that capitalizes the first letter and lowercases the remaining letters in a given string.py
@@ -0,0 +1,8 @@
+# Write a Python program that capitalizes the first letter and lowercases the remaining letters in a given string.
+# Sample Data:
+# ("Red Green WHITE") -> "Red Green White"
+# ("w3resource") -> "W3resource"
+# ("dow jones industrial average") -> "Dow Jones Industrial Average"
+
+s = input()
+print(s.title())
