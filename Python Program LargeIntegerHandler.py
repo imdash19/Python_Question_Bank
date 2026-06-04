0046e260303feec2fad304ee365dd00675547cdb
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-06-04 19:31:46 +0530

    Add files via upload

---
+++ Python Program LargeIntegerHandler.py
@@ -0,0 +1,5 @@
+# Reads extremely large integers (up to 10^18) as input. The program handles these large values without precision loss, often treating them as strings or using appropriate numeric types.
+
+large_number = int(input())
+
+print(large_number)
