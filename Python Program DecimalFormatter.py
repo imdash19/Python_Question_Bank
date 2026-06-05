63e697e6e1a5e40582785907b9360f99b3da6a4f
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-06-05 20:28:17 +0530

    Add files via upload

---
+++ Python Program DecimalFormatter.py
@@ -0,0 +1,5 @@
+# Reads a decimal number and formats it to exactly two decimal places using the .format() method. Demonstrates precision control in numeric formatting.
+
+number = float(input())
+
+print("{:.2f}".format(number))
