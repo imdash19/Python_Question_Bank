04f9f4eedc2d79af7af597ea13fb93fbd0ba7b0a
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-06-06 15:55:31 +0530

    Add files via upload

---
+++ Python Program WidthAlignedIntegerFormatter.py
@@ -0,0 +1,5 @@
+# Reads an integer and formats it with specific width alignment using .format() method. Demonstrates field width control for consistent output layout.
+
+n= int(input('Please enter your number: '))
+
+print('"Number: {}"'.format(n))
