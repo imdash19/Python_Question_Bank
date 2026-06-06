77c109cbdc3be11ac9dc7eda1e4e064dd6dbbad3
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-06-06 15:57:44 +0530

    Add files via upload

---
+++ Python Program PercentWidthFormatter.py
@@ -0,0 +1,5 @@
+# Reads an integer and formats it with specific width alignment using %-formatting operator. Shows traditional width formatting approach.
+
+n= int(input('Please enter your number: '))
+
+print('"Number: %d"' % n)
