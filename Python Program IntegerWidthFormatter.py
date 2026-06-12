3ccebb84918fc06eafb771d63b3e00a28a681bf9
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-06-12 17:27:20 +0530

    Update comment formatting in IntegerWidthFormatter.py

---
+++ Python Program IntegerWidthFormatter.py
@@ -0,0 +1,5 @@
+# Reads an integer and formats it with specific width alignment using %-formatting operator. Shows traditional width formatting approach.
+
+number = int(input())
+
+print("%10d" % number)
