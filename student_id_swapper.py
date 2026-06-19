c2e6ed676af0011c480b0b17ca6c9331bc46af8d
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-06-19 20:01:07 +0530

    Implement student ID swapping functionality

---
+++ student_id_swapper.py
@@ -0,0 +1,9 @@
+# Swap two student ID values and print updated values
+
+student_id1 = int(input())
+student_id2 = int(input())
+
+student_id1, student_id2 = student_id2, student_id1
+
+print(student_id1)
+print(student_id2)
