7eb1b3db849b2fe507194c1e4d3c0b2cfc091f17
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-06-20 19:36:58 +0530

    Fix class instantiation and add missing newline

---
+++ mobile_details.py
@@ -0,0 +1,10 @@
+# Create a Mobile class with attributes model and price then create object and display values
+
+class Mobile:
+    def __init__(self, model, price):
+        self.model= model
+        self.price= price
+
+m= Model(input(), int(input()))
+print(m.model)
+print(m.price)
