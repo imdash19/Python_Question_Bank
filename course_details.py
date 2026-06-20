a693e55ed59ad7ea430cb9f5c5e9c770b33d84f9
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-06-20 19:39:45 +0530

    Implement Course class with attributes and instantiation

---
+++ course_details.py
@@ -0,0 +1,13 @@
+# Create a Course class with attributes:
+# -> course_name
+# -> duration
+# Create an object of the class.
+# Print the values of the attributes.
+
+class Course:
+    def __init__(self, course_name, duration):
+        self.course_name= course_name
+        self.duration= duration
+
+c= Course(input(), input())
+print(c.course_name, c.duration)
