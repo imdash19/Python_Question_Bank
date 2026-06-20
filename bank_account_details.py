b222dcbe12925f355ab4ee84fa28ef1e0c830102
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-06-20 19:34:30 +0530

    Add BankAccount class to manage account details

---
+++ bank_account_details.py
@@ -0,0 +1,10 @@
+# Create a BankAccount class with attributes account_number and balance then create object and print them
+
+class BankAccount:
+  def __init__(self, account_number, balance):
+    self.account_number= account_number
+    self.balance= balance
+
+account= BankAccount(int(input(), float(input()))
+print(account.account_number)
+print(account.balance)
