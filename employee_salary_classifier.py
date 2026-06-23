66e9723bec2716e1d3aa305e506760f7cc8b5861
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-06-23 20:36:56 +0530

    Implement Employee class to classify salary

---
+++ employee_salary_classifier.py
@@ -0,0 +1,16 @@
+# Create an Employee class with a parameterized constructor.
+# The constructor should accept salary and print:
+# "High Salary" if salary > 30000
+# "Normal Salary" if salary ≤ 30000
+
+class Employee:
+    def __init__(self, salary):
+        self.salary= salary
+
+e= Employee(float(input()))
+
+if e.salary > 30000:
+    print("High Salary")
+
+else:
+    print("Normal Salary")
