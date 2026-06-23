25f0ce43758b6f024db3e6b35c90e2f21b7faeb8
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-06-23 20:28:42 +0530

    Implement BankAccount class to check balance

---
+++ bank_account_balance_checker.py
@@ -0,0 +1,13 @@
+# Create BankAccount class with balance attribute and print Sufficient Balance if >=500
+
+class BankAccount:
+    def __init__(self, balance):
+        self.balance= balance
+
+account= BankAccount(float(input()))
+
+if account.balance >= 500:
+    print('Sufficient Balance')
+
+else:
+    print('Low Balance')
