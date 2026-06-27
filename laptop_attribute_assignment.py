5f63daa2d2ff857fa06ef85cdc5fc22217a45969
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-06-27 19:29:44 +0530

    Implement Laptop class with brand and RAM attributes
    Added a Laptop class with attributes for brand and RAM, and created an instance to display these attributes.

---
+++ laptop_attribute_assignment.py
@@ -0,0 +1,15 @@
+# Create a Laptop class with attributes:
+# brand
+# ram
+# Create an object of the class.
+# Assign attributes after object creation.
+# Print the details.
+
+class Laptop:
+    def __init__(Self, brand, ram):
+        self.brand= brand
+        self.ram= ram
+
+l= Laptop(input(), input())
+print(l.brand)
+print(l.ram)
